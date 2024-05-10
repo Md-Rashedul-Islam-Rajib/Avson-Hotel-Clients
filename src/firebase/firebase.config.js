a6d30cff1ee8f41df311c7f80b7573aff212dbda
rajib5570 <rajib5570@gmail.com>
2024-05-11 00:52:58 +0600

    .env.local file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBTv2jHpHlSgERwzI2wIwSS39N-LZ13Dr0",
-  authDomain: "assignment-11-fb88e.firebaseapp.com",
-  projectId: "assignment-11-fb88e",
-  storageBucket: "assignment-11-fb88e.appspot.com",
-  messagingSenderId: "529375043960",
-  appId: "1:529375043960:web:8930b8f71189345f4e288d"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+export default auth;
