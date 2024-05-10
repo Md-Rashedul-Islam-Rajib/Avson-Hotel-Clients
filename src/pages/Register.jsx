3737387d34143766701d3e961faf495244bc8e42
rajib5570 <rajib5570@gmail.com>
2024-05-11 00:21:07 +0600

    register page added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { IoImagesOutline } from "react-icons/io5";
 
 const Register = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
 
   // const { logInUser, setUser, googleLogin, githubLogin, user,theme } =
   //   useContext(AuthContext);
@@ -80,47 +82,36 @@ const Register = () => {
             <div className="card shrink-0 w-full max-w-sm shadow-2xl text-black p-8 bg-base-100">
               <h2 className="py-8">Welcome to Avson</h2>
               <form onSubmit={handleSubmit(onSubmit)}>
-                <p className="mb-2">Email</p>
-                <label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
-                  <input
-                    type="text"
-                    className="grow"
-                    placeholder="Email"
-                    {...register("email", { required: true })}
-                  />
-                  {errors.email && (
-                    <span className="text-red-600 text-sm">
-                      Email is required
-                    </span>
-                  )}
-                </label>
 
-                <p className="mb-2">password</p>
-                <label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
-                  <input
-                    type={showPassword ? "text" : "password"}
-                    className="grow"
-                    placeholder="Password"
-                    {...register("password", { required: true })}
-                  />
-                  <span
-                    onClick={() => {
-                      setShowPassword(!showPassword);
-                    }}
-                  >
-                    {showPassword ? <FaEyeSlash /> : <FaEye />}
-                  </span>
-                  {errors.password && (
-                    <span className="text-red-600 text-sm">
-                      Password is required
-                    </span>
-                  )}
-                </label>
+                <p>Name</p>
+<label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
+  <input type="text" className="grow" placeholder="Name" {...register("Name")} />
+</label>
 
-                <button className={`btn text-white bg-[#FEA116] w-full`}>
-                  Log In
-                </button>
-              </form>
+            
+
+<p>Email</p>
+<label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
+  <input type="text" className="grow" placeholder="Email" {...register('email', { required: true })}/>
+</label>
+  {errors.email && <p className="text-red-600 text-sm">Email is required</p>}
+
+<p>Photo URL</p>
+<label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
+  <input type="text" className="grow" placeholder="Photo URL" {...register('PhotoURL')} />
+  
+</label>
+
+<p>Password</p>
+<label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
+  <input type={showPassword ? 'text' : 'password'} className="grow" placeholder="Password" {...register('password', { required: true })}/>
+  <span onClick={()=> {setShowPassword(!showPassword)}}>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
+</label>
+  {errors.password && <p className="text-red-600 text-sm">Password is required</p>}
+  {error && <p className="text-red-600 text-sm">{error}</p>}
+
+<button className={`btn text-white bg-[#FEA116] w-full`}>Register</button>
+</form>
             </div>
           </div>
         </div>
