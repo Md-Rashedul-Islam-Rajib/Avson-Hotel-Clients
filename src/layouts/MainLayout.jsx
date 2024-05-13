09de4d56a508ccb57852cb86f82dc1112fb3ac83
rajib5570 <rajib5570@gmail.com>
2024-05-13 14:21:02 +0600

    footer section added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,12 +2,15 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
+import Footer from '../components/Footer';
+
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
             <Toaster />
         </div>
     );
